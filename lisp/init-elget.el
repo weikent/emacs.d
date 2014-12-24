902adce763b88c28e0ce3ed9091970e6860c7203
weikent <wei.shijian@gmail.com>
2014-12-24 15:26:20 +0800

    el-get 使用 作者github中的新的说明。 elpa继续使用marmalade

--- lisp/init-elget.el
+++ lisp/init-elget.el
@@ -14,13 +14,26 @@
 (message "start el-get")
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
+;; (unless (require 'el-get nil 'noerror)
+;;   (with-current-buffer
+;;       (url-retrieve-synchronously
+;;        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+;;     (let (el-get-master-branch)
+;;       (goto-char (point-max))
+;;       (eval-print-last-sexp))))
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (let (el-get-master-branch)
-      (goto-char (point-max))
-      (eval-print-last-sexp))))
+       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
+
 
 (setq my:el-get-packages
       '(
